f21e3c57d800c68f4de2190910884b353cfc62a1
Bhintuna <bhintunash@gmail.com>
2025-02-20 12:56:46 +0545

    Path from environment variable

--- Controllers/ConnStringController.cs
+++ Controllers/ConnStringController.cs
@@ -1,11 +1,8 @@
-﻿using DocumentFormat.OpenXml.InkML;
-using DocumentFormat.OpenXml.Spreadsheet;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SAAS_Query_API.Data;
 using SAAS_Query_API.Services;
-using System.Runtime.InteropServices;
 
 namespace SAAS_Query_API.Controllers
 {
@@ -14,33 +11,35 @@ namespace SAAS_Query_API.Controllers
     public class ConnStringController : ControllerBase
     {
         private readonly MyDBContext _myDBContext;
+        private readonly string? _path;
         
 
-        public ConnStringController(MyDBContext myDBContext)
+        public ConnStringController(MyDBContext myDBContext, IConfiguration configuration)
         {
-            _myDBContext = myDBContext; 
-
+            _myDBContext = myDBContext;
+            _path = configuration["AppSettings:FolderPath"];
         }
 
-        async Task  RunQueryFromFilesAsync(List<string> connectionStringFormatArray, string path)
+        string GetPath()
+        {
+            //string? path = Environment.GetEnvironmentVariable("Folder_Path");
+            if (string.IsNullOrEmpty(_path)){
+                throw new Exception("There is no given path in this device");
+            }
+            Console.WriteLine($"{ _path} is here");
+            return _path;
+        }
+
+        async Task  RunQueryFromFilesAsync(List<string> connectionStringFormatArray)
         {
             try
             {
+                string path=GetPath();
                 IEnumerable<string> txtFiles;
-                //if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                //{
-                //    Console.WriteLine("Linux");
-                //    txtFiles = Directory.EnumerateFiles(path);
-                //    Console.WriteLine($"The path is {path}");
-                //    //txtFiles = Directory.EnumerateFiles(@"/dev/sdh/DOT NET INTERNSHIPSAAS-Project/SAAS Query API/SQL Query Files Folder/", "*.txt");//linux
-                //}
-                //else
-                //{
-                    Console.WriteLine("Windows");
-                    //txtFiles = Directory.EnumerateFiles(path);
-                    Console.WriteLine($"The path is {path}");
-                    txtFiles = Directory.EnumerateFiles(@"H:\DOT NET INTERNSHIP\SAAS-Project\SAAS Query API\SQL Query Files Folder\", "*.txt"); //windows
-                //}
+                //Console.WriteLine("Windows");
+                //txtFiles = Directory.EnumerateFiles(path);
+                Console.WriteLine($"The path is {path}");
+                txtFiles = Directory.EnumerateFiles(@"H:\DOT NET INTERNSHIP\SAAS-Project\SAAS Query API\SQL Query Files Folder\", "*.txt"); //windows
 
                 foreach (string currentFile in txtFiles)
                 {
@@ -66,9 +65,8 @@ namespace SAAS_Query_API.Controllers
             
         }
 
-        //[HttpGet]
-        [HttpGet("{path}")]
-        public async Task<ActionResult> GetConnectionString(string path)
+        [HttpGet]
+        public async Task<ActionResult> GetConnectionString()
         {
             string connectionStringformat;
             List<string> connectionStringFormatArray=new List<string>();
@@ -89,15 +87,14 @@ namespace SAAS_Query_API.Controllers
                 bool TrustServerCertificate = true;
 
                 foreach (var col in ServerDBInfoList)
-                {  
-                    //connectionStringformat = $"Data Source={col.connStringServerName};Initial Catalog={col.connStringDatabaseName};Integrated Security={IntegratedSecurity};Trust Server Certificate={TrustServerCertificate}";
+                {      
                     connectionStringformat = $"Data Source={col.connStringServerName};Initial Catalog={col.connStringDatabaseName};User ID = {col.connStringUserName}; Password = {col.connStringPassword}; Integrated Security={IntegratedSecurity};Trust Server Certificate={TrustServerCertificate}";
                     Console.WriteLine($"The connection string is : {connectionStringformat}");
                     connectionStringFormatArray.Add(connectionStringformat);
                 }
 
     
-                await RunQueryFromFilesAsync(connectionStringFormatArray, path);
+                await RunQueryFromFilesAsync(connectionStringFormatArray);
                 
                 return Ok();
             }
