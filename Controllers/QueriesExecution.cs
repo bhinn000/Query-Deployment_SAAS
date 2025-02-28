46c483e6ae60fde2332d97d8fbbedbdd4f4a6fd2
Bhintuna <bhintunash@gmail.com>
2025-02-28 14:55:45 +0545

    Logged the output as per required

--- Controllers/QueriesExecution.cs
+++ Controllers/QueriesExecution.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SAAS_Query_API.Data;
 using SAAS_Query_API.Services;
@@ -10,6 +11,7 @@ namespace SAAS_Query_API.Controllers
         private readonly MyDBContext _myDBContext;
         private readonly string? _path;
         private readonly ILogger<QueriesExecution> _logger;
+        int totalFilesInFolder;
 
         public QueriesExecution(MyDBContext myDBContext, IConfiguration configuration, ILogger<QueriesExecution> logger)
         {
@@ -31,11 +33,63 @@ namespace SAAS_Query_API.Controllers
         }
 
         IEnumerable<string> txtFiles;
-        async Task RunQueryFromFilesAsync(string connectionStringFormat , string dbName)
+        Dictionary<string, Dictionary<int, string>> errorDetail = new Dictionary<string, Dictionary<int, string>>();
+
+        //async Task RunQueryFromFilesAsync(string connectionStringFormat , string dbName)
+        //{
+        //    IEnumerable<string> txtFiles = GetPathAndTextFiles();
+        //    int filePosition = 1;
+        //    int totalFilesInFolder = txtFiles.Count();
+        //    List<int> filesWithError = new List<int>();
+        //    List<int> filesRunSuccessfully = new List<int>();
+        //    foreach (string currentFile in txtFiles)
+        //    {
+        //        using StreamReader streamReader = new StreamReader(currentFile);
+        //        string fromTextFile = await streamReader.ReadToEndAsync();
+        //        try
+        //        {
+        //            using (SqlConnection conn = new SqlConnection(connectionStringFormat))
+        //            {
+        //                string query = fromTextFile;
+
+        //                SqlCommand cmd1 = new SqlCommand(query, conn);
+        //                await cmd1.Connection.OpenAsync();
+
+        //                SqlDataReader retrievedValue = await cmd1.ExecuteReaderAsync();
+        //                filesRunSuccessfully.Append(filePosition);
+        //                if(filesRunSuccessfully.Count == totalFilesInFolder)
+        //                {
+        //                    string allOrNot = "All";
+        //                }
+        //                //_logger.LogInformation($"Database Name:  {dbName} \n {filePosition} executed sucessfully");
+        //                filePosition++;
+        //                if(filePosition > totalFilesInFolder)
+        //                {
+        //                    //_logger.LogInformation($"****Queries from all files run successfully in database named {dbName}****");
+        //                }
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            //_logger.LogError($"Queries from FileNo. \'{filePosition}\' couldn't run in database named {dbName}; \n Error Detail: {ex.Message}");
+        //            filesWithError.Append(filePosition);
+        //            filePosition++;
+        //        }
+
+        //        _logger.LogInformation($"{filesRunSuccessfully.Count()} executed sucessfully");
+        //    }
+
+
+        //}
+
+        async Task<int> RunQueryFromFilesAsync(string connectionStringFormat, string dbName)
         {
             IEnumerable<string> txtFiles = GetPathAndTextFiles();
             int filePosition = 1;
-            int totalFilesInFolder = txtFiles.Count();
+            totalFilesInFolder = txtFiles.Count();
+            List<int> filesWithErrorInThisDB = new List<int>();
+            List<int> filesRunSuccessfullyInThisDB = new List<int>();
+
             foreach (string currentFile in txtFiles)
             {
                 using StreamReader streamReader = new StreamReader(currentFile);
@@ -50,27 +104,67 @@ namespace SAAS_Query_API.Controllers
                         await cmd1.Connection.OpenAsync();
 
                         SqlDataReader retrievedValue = await cmd1.ExecuteReaderAsync();
-
-                        _logger.LogInformation($"Database Name:  {dbName} \n {filePosition} executed sucessfully");
-                        filePosition++;
-                        if(filePosition > totalFilesInFolder)
-                        {
-                            _logger.LogInformation($"****Queries from all files run successfully in database named {dbName}****");
-                        }
+                        filesRunSuccessfullyInThisDB.Add(filePosition);
+                  
                     }
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError($"Queries from FileNo. \'{filePosition}\' couldn't run in database named {dbName}; \n Error Detail: {ex.Message}");
-                    filePosition++;
+                    filesWithErrorInThisDB.Add(filePosition);
+                    errorDetail[dbName] = new Dictionary<int, string>();
+                    errorDetail[dbName][filePosition] = ex.Message;
                 }
+
+                filePosition++;
+
             }
+
+            return filesRunSuccessfullyInThisDB.Count;
+
         }
+
+        //public async Task GetConnectionString()
+        //{
+        //    string connectionStringformat;
+        //    List<string> connectionStringFormatArray=new List<string>();
+        //    ConnectionStringEnt cse = new ConnectionStringEnt();
+
+        //    try
+        //    {
+        //        List<ConnectionStringEnt> connStringList = await _myDBContext.COMPANY_DATABASE_INFO.ToListAsync();
+        //        var ServerDBInfoList = connStringList.Select(each => new
+        //        {
+        //            connStringServerName = each.SERVERNAME,
+        //            connStringDatabaseName = each.DATABASENAME,
+        //            connStringUserName= each.DBUSER,
+        //            connStringPassword = EncryptionHelper.Encrypt(EncryptionHelper.FromHexString((each.DBPASSWORD).ToString()))
+        //        }).ToList();
+
+        //        bool IntegratedSecurity = false;
+        //        bool TrustServerCertificate = true;
+
+        //        foreach (var col in ServerDBInfoList)
+        //        {      
+        //            connectionStringformat = $"Data Source={col.connStringServerName};Initial Catalog={col.connStringDatabaseName};User ID = {col.connStringUserName}; Password = {col.connStringPassword}; Integrated Security={IntegratedSecurity};Trust Server Certificate={TrustServerCertificate}";
+        //            await RunQueryFromFilesAsync(connectionStringformat, col.connStringDatabaseName);
+        //            //_logger.LogInformation($"****Queries from all files run successfully in database named {dbName}****");
+        //        }
+
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        _logger.LogError($"\nError detail: {ex.Message}");
+
+        //    }
+
+        //}
+         
 
         public async Task GetConnectionString()
         {
             string connectionStringformat;
-            List<string> connectionStringFormatArray=new List<string>();
+            int DBWithAllFileSuccess = 0;
+            List<string> connectionStringFormatArray = new List<string>();
             ConnectionStringEnt cse = new ConnectionStringEnt();
 
             try
@@ -80,7 +174,7 @@ namespace SAAS_Query_API.Controllers
                 {
                     connStringServerName = each.SERVERNAME,
                     connStringDatabaseName = each.DATABASENAME,
-                    connStringUserName= each.DBUSER,
+                    connStringUserName = each.DBUSER,
                     connStringPassword = EncryptionHelper.Encrypt(EncryptionHelper.FromHexString((each.DBPASSWORD).ToString()))
                 }).ToList();
 
@@ -88,19 +182,53 @@ namespace SAAS_Query_API.Controllers
                 bool TrustServerCertificate = true;
 
                 foreach (var col in ServerDBInfoList)
-                {      
+                {
                     connectionStringformat = $"Data Source={col.connStringServerName};Initial Catalog={col.connStringDatabaseName};User ID = {col.connStringUserName}; Password = {col.connStringPassword}; Integrated Security={IntegratedSecurity};Trust Server Certificate={TrustServerCertificate}";
-                    await RunQueryFromFilesAsync(connectionStringformat, col.connStringDatabaseName);
+
+                    int filesExecutedNum= await RunQueryFromFilesAsync(connectionStringformat, col.connStringDatabaseName);
+
+                    if (filesExecutedNum == totalFilesInFolder)
+                    {
+                        DBWithAllFileSuccess++;
+                    }
+                    else
+                    {
+                        errorDetail[col.connStringDatabaseName][-1] = filesExecutedNum.ToString();
+                    }
+
                 }
-   
+
+                if (DBWithAllFileSuccess == ServerDBInfoList.Count())
+                {
+                    _logger.LogInformation($"Run all queries successfully in all {DBWithAllFileSuccess} databases****");
+                }
+                else
+                {
+                    foreach (var db in errorDetail)
+                    {
+                        Console.WriteLine($"Database: {db.Key} \n {db.Value[-1]} files executed successfully");
+                        foreach (var error in db.Value)
+                        {
+                            if (error.Key != -1)
+                            {
+                                Console.WriteLine($"  FileNo {error.Key}: {error.Value}");
+                            }
+                           
+                        }
+                    }
+                    _logger.LogInformation($"All queries run successfully only in {DBWithAllFileSuccess} databases****");  
+                }
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogError($"\nError detail: {ex.Message}");
- 
+
             }
 
         }
+
+
     }
 }
 
